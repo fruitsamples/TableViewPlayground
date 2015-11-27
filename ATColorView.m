5ed82634993142b5b3b46ee8c2c1f418e514d05b
Steve Moser <contact@stevemoser.org>
2015-11-26 21:01:34 -0500

    Add OSX SDK 10.7 (411.5) Docset changes

---
+++ ATColorView.m
@@ -0,0 +1,119 @@
+/*
+     File: ATColorView.m
+ Abstract: A basic NSView subclass that supports having an animatable background color (NOTE: the animation only works when the view is layer backed).
+ 
+  Version: 1.2
+ 
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2011 Apple Inc. All Rights Reserved.
+ 
+ */
+
+#import "ATColorView.h"
+
+#import <Quartz/Quartz.h>
+
+@implementation ATColorView
+
++ (id)defaultAnimationForKey:(NSString *)key {
+    if ([key isEqualToString:@"backgroundColor"]) {
+        return [CABasicAnimation animation];
+    }
+    return [super defaultAnimationForKey:key];
+}
+
+- (void)dealloc {
+    self.backgroundColor = nil;
+    [super dealloc];
+}
+
+
+@synthesize backgroundColor;
+@synthesize drawBorder;
+
+- (CGColorRef)createBackgroundColorRef {
+    // Create a CGColorRef from the NSColor and set that on our layer
+    CGFloat components[backgroundColor.numberOfComponents];
+    [backgroundColor getComponents:components];
+    return CGColorCreate([[backgroundColor colorSpace] CGColorSpace], components);
+}
+
+- (void)setBackgroundColor:(NSColor *)value {
+    if (backgroundColor != value) {
+        [backgroundColor release];
+        backgroundColor = [value retain];
+        if (self.layer == nil) {
+            CGColorRef backgroundColorRef = [self createBackgroundColorRef];
+            self.layer.backgroundColor = backgroundColorRef;
+            CGColorRelease(backgroundColorRef);
+        }
+        [self setNeedsDisplay:YES];
+    }
+}
+
+- (void)drawRect:(NSRect)r {
+    NSColor *color = [self backgroundColor];
+    if (color) {
+        [color set];
+        NSRectFill(r);
+    }
+    if (self.drawBorder) {
+        [[NSColor lightGrayColor] set];
+        NSFrameRectWithWidth(self.bounds, 1.0);
+    }
+    if (self.window.firstResponder == self) {
+        NSSetFocusRingStyle(NSFocusRingOnly);
+        NSRectFill(self.bounds);
+    }
+}
+
+- (void)mouseUp:(NSEvent *)theEvent {
+    NSPoint point = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+    if (NSPointInRect(point, self.bounds) && self.action) {
+        [NSApp sendAction:self.action to:self.target];
+    }
+}
+
++ (Class)cellClass {
+    // The cell is a container for the target/action
+    return [NSActionCell class];
+}
+
+@end
